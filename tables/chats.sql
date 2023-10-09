371ced8b0c81c976031030036acdab3feb01c269
Pasha Semenov <paul23093@yandex.ru>
2023-10-09 13:31:08 +0300

    Create chats.sql

---
+++ tables/chats.sql
@@ -0,0 +1,9 @@
+create table chats (
+	id int GENERATED ALWAYS AS identity,
+	tg_chat_id float not null,
+	tg_chat_name varchar(50),
+	tg_chat_photo varchar(50000),
+	created_at timestamp default current_timestamp,
+	updated_at timestamp default current_timestamp,
+	constraint pk_chat unique(tg_chat_id)
+)
