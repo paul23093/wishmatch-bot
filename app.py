d04012f0824129943e5713f36c805ffde5e08611
psemenov <pavel.semenov@exness.com>
2023-11-12 23:32:31 +0200

    Minor changes

--- app.py
+++ app.py
@@ -563,8 +563,6 @@ async def select_santa_chat(update: Update, context: ContextTypes.DEFAULT_TYPE) 
         reply_markup=reply_markup
     )
 
-    context.chat_data["secret_santa_list"] = []
-
 
 async def publish_secret_santa(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message = update.effective_message
@@ -613,6 +611,8 @@ async def publish_secret_santa(update: Update, context: ContextTypes.DEFAULT_TYP
         )
 
     elif query.data == "join":
+        if not context.chat_data["secret_santa_list"]:
+            context.chat_data["secret_santa_list"] = []
         context.chat_data["secret_santa_list"].append(query.from_user.username)
         await query.message.edit_text(
             text=query.message.text + f"\n\nParticipants: {context.chat_data['secret_santa_list']}"
